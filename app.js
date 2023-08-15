c2097c579db93d59cb4e33fdf52cf1f2601c2b38
Artem Smolin <krutopognali@yandex.ru>
2023-08-15 14:49:55 +0300

    refactor: auth middleware off

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const PORT = 3000;
 const express = require('express');
 const mongoose = require('mongoose');
 
-const { auth } = require('./middleware/auth');
+/* const { auth } = require('./middleware/auth'); */
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 
@@ -13,7 +13,16 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb')
 const app = express();
 
 app.use(express.json());
-app.use(auth);
+/* app.use(auth); */
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '64da107d3ebb24fe1d2fee97',
+  };
+
+  next();
+});
+
 app.use('/users', userRouter);
 app.use('/cards', cardRouter);
 
