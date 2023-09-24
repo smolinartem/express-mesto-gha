594ad64a892c312718815a0bb54bf1f1cf5da62a
Artem Smolin <krutopognali@yandex.ru>
2023-09-24 18:56:52 +0300

    refactor: models changed

--- controllers/cards.js
+++ controllers/cards.js
@@ -18,7 +18,7 @@ const createNewCard = async (req, res, next) => {
   try {
     const { name, link } = req.body;
     const owner = req.user._id;
-    const card = await Cards.create({ name, link, owner });
+    const card = await Cards.create({ name, link, owner }, { runValidators: true });
     res.status(201).send({ card });
   } catch (err) {
     if (err instanceof Error.ValidationError) {
